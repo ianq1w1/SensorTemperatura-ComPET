92c36843a2f9abf7eed7f7f663227b76bb4d614d
ianq1w1 <ianpereiraian2@gmail.com>
2025-05-02 19:50:01 -0300

    adição de chave e junção de time sharing no codigo com LCD

--- Projeto-Temperatura-Umidade-Com-LCD_V2/Projeto-Temperatura-Umidade-Com-LCD_V2.ino
+++ Projeto-Temperatura-Umidade-Com-LCD_V2/Projeto-Temperatura-Umidade-Com-LCD_V2.ino
@@ -1,35 +1,55 @@
 #include <DHT.h>
 #include <LiquidCrystal.h>
+#include <TimerOne.h>
 
 #define DHTPIN 6 //Pino conectado ao DHT22
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
 // LCD: RS, EN, D4, D5, D6, D7
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+volatile bool startSystem = false;
+volatile byte Proccess_count = 0;
 
 float tempMin = 10.0;
 float tempMax = 70.0;
 float umidMin = 12.0;
 float umidMax = 90;
 
+unsigned long tempoAgora = 0;
+unsigned long sensorTempoAgora = 0;
+
+float temperatura = 0;
+float umidade = 0;
+
+bool abaixoTemp = false;
+bool acimaTemp = false;
+bool abaixoUmid = false;
+bool acimaUmid = false;
+
 #define LED_NORMAL 8
 #define LED_ABAIXO 9
 #define LED_ACIMA 10
+#define CHAVE_PIN 2
+
+void handlerflag(){
+  startSystem = !startSystem;  
+}
+
+void handler(){
+  Proccess_count++;
+}
 
 void setup() {
   //Pinos do LCD
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(11,OUTPUT);
-  pinMode(12,OUTPUT);
+
 
   pinMode(LED_NORMAL, OUTPUT);
   pinMode(LED_ABAIXO, OUTPUT);
   pinMode(LED_ACIMA, OUTPUT);
+  pinMode(CHAVE_PIN, INPUT_PULLUP);
   
   Serial.begin(9600);
   dht.begin();
@@ -37,68 +57,115 @@ void setup() {
   lcd.print("Iniciando...");
   delay(2000);
   lcd.clear();
+
+  Timer1.initialize(1000000);
+  attachInterrupt(digitalPinToInterrupt(CHAVE_PIN), handlerflag, CHANGE);
+  tempoAgora = millis();
+  sensorTempoAgora = millis();
+}
+
+void Proccess0(){ 
+ 
+  temperatura = dht.readTemperature();
+  umidade = dht.readHumidity();
+  
+  if((millis() - sensorTempoAgora) > 2000){
+     sensorTempoAgora = millis();
+     lcd.clear(); // <- Adicionado
+      lcd.setCursor(0,0);
+      lcd.print("T:");
+      lcd.print(temperatura,1);
+      lcd.print((char)223); //Simbolo do celsius
+      lcd.print("C ");
+
+      lcd.setCursor(0,1);
+      lcd.print("U:");
+      lcd.print(umidade,1);
+      lcd.print("% ");
+  }
+}
+
+void Proccess1(){
+  
+  if ((millis() - tempoAgora) > 3000) {
+    tempoAgora = millis(); // Atualiza tempo só uma vez
+
+    //VERIFICA TEMPERATURA
+    if (temperatura < tempMin) {
+      abaixoTemp = true;
+    }else{
+       abaixoTemp = false; 
+    }
+    if(temperatura > tempMax){
+       acimaTemp = true;
+    }else{
+      acimaTemp = false;
+    }
+
+    //VERIFICA UMIDADE
+    if (umidade < umidMin) {
+       abaixoUmid = true;
+    }else{
+       abaixoUmid = false; 
+    }
+    if(umidade > umidMax){
+       acimaUmid = true;
+    }else{
+      acimaUmid = false;
+    }
+    
+    if (acimaUmid || acimaTemp) {
+      Serial.println("Alerta de limite excedido:");
+        lcd.clear(); // <- Adicionado
+      if (acimaTemp) {
+        lcd.setCursor(0,0);
+        lcd.print("!TEMP acima!");
+      }
+      if (acimaUmid) {
+        lcd.setCursor(0,1);
+        lcd.print("!UMID acima!");
+      }
+      digitalWrite(LED_ACIMA, HIGH);
+    }else{
+      digitalWrite(LED_ACIMA, LOW);
+    }
+
+    if (abaixoUmid || abaixoTemp){
+        lcd.clear(); // <- Adicionado
+      if (abaixoUmid){
+        lcd.setCursor(0,1);
+        lcd.print("!UMID abaixo!");        
+      }
+      if (abaixoTemp){
+        lcd.setCursor(0,0);
+        lcd.print("!TEMP abaixo!");
+      }
+      digitalWrite(LED_ABAIXO, HIGH);
+     } else {
+      digitalWrite(LED_ABAIXO, LOW);
+    }
+
+    // Validação do sensor
+    if (isnan(temperatura) || isnan(umidade)) {
+      Serial.println("Falha na leitura do sensor DHT!");
+    }
+  }
 }
 
 void loop() {
-  delay(1000);
-
- float temperatura = dht.readTemperature();
- float umidade = dht.readHumidity();
-
-  bool abaixoTemp = temperatura < tempMin;
-  bool acimaTemp = temperatura > tempMax;
-  bool abaixoUmid = umidade < umidMin;
-  bool acimaUmid = umidade > umidMax;
-  
-  Serial.print("Temperatura:");
-  Serial.print(temperatura);
-  Serial.print("C, Umidade:");
-  Serial.println(umidade);
-
-  lcd.clear();
-  
-  if(abaixoTemp){
-    Serial.println("Temperatura abaixo do ideal");
-    lcd.setCursor(0,0);
-    lcd.print("!TEMP abaixo!");
-  }else if(acimaTemp){
-    Serial.println("Temperatura acima do ideal");
-    lcd.setCursor(0,0);
-    lcd.print("!TEMP acima!");
-  }else{
-    lcd.setCursor(0,0);
-    lcd.print("T:");
-    lcd.print(temperatura,1);
-    lcd.print((char)223); //Simbolo do celsius
-    lcd.print("C ");
+  //delay(200);
+  if(startSystem == true){
+    Timer1.attachInterrupt(handler);
+    
+      switch(Proccess_count){
+        case 0: Proccess0();
+                break;
+        case 1: Proccess1();
+                break;
+        default: Proccess_count = 0;
+                 break;
+      }
   }
 
-  if(abaixoUmid){
-    Serial.println("Umidade abaixo do ideal");
-    lcd.setCursor(0,1);
-    lcd.print("!UMID abaixo!");
-  }else if(acimaUmid){
-    Serial.println("Umidade acima do ideal");
-    lcd.setCursor(0,1);
-    lcd.print("!UMID acima!");
-  }else {
-    lcd.setCursor(0,1);
-    lcd.print("U:");
-    lcd.print(umidade,1);
-    lcd.print("% ");
-  }
-
-  if(abaixoTemp || abaixoUmid){
-    digitalWrite(LED_NORMAL, LOW);
-    digitalWrite(LED_ABAIXO, HIGH);
-    digitalWrite(LED_ACIMA, LOW);
-  }else if(acimaTemp || acimaUmid){
-    digitalWrite(LED_NORMAL, LOW);
-    digitalWrite(LED_ABAIXO, LOW);
-    digitalWrite(LED_ACIMA, HIGH);
-  }else{
-    digitalWrite(LED_NORMAL, HIGH);
-    digitalWrite(LED_ABAIXO, LOW);
-    digitalWrite(LED_ACIMA, LOW);
-  }
+  //Serial.println("teste");
 }
